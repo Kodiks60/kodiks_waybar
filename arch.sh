9260c5361793f9cf6524066dbf8bc5055e8b5f06
Kodiks60 <kodiks360@gmail.com>
2026-03-22 21:11:30 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,4 +1,4 @@
-sudo pacman -S waybar ttf-jetbrains-mono ttf-dejavu hyprpaper
+sudo pacman -S waybar ttf-jetbrains-mono ttf-dejavu hyprpaper ttf-font-awesome ttf-nerd-fonts-symbols
 
 cp -r waybar /home/$USER/.config
 cp hyprpaper.conf /home/$USER/.config/hypr/
